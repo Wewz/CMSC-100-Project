93ede5701e71e27ae153078657eadf9203f41ded
Wewz <jacentino@up.edu.ph>
2023-12-17 18:16:19 +0800

    feat: Product List create

--- proj/src/UI Design/Homepage Signed In/productList.jsx
+++ proj/src/UI Design/Homepage Signed In/productList.jsx
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Products = () => {
 
+
+    const [ products, setProducts] = useState([])
+    //const [ greeting, setGreeting ] = useState('')
+    useEffect(() => {
+    fetch('http://localhost:3001/get-product')
+      .then(response => response.json())
+      .then(body => {
+    setProducts(body)
+      })
+    }, [])
+
     return(
         <div>
-        
+
 
 
             <div className="hidden xl:flex 2xl:flex lg:flex md:flex justify-between items-center text-center mt-24 w-[80%] ml-[10%] pb-7 border-b border-[#2D4944]">
@@ -18,10 +29,36 @@ const Products = () => {
                 </div>
 
                 <div className="flex gap-2 h-full">
-                    <button className="bg-[#2D4944] h-10 w-24 text-sm text-white rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">Default</button>
-                    <button className="bg-[#2D4944] h-10 w-24 text-sm text-white rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">A-Z</button>
-                    <button className="bg-[#2D4944] h-10 w-24 text-sm text-white rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">Price Cost</button>
+                    <button className="bg-[#2D4944] font-bold h-10 w-24 text-sm text-white rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">Default</button>
+                    <button className="bg-white font-bold h-10 w-24 border border-[#2D4944] text-sm text-[#2D4944] rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">A-Z</button>
+                    <button className="bg-white font-bold h-10 w-24 border border-[#2D4944] text-sm text-[#2D4944] rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">Price Cost</button>
                 </div>
+            </div>
+
+
+            <div>
+                {
+                products.map((product, index) => (
+                        <div key={product.ptitle}>
+                            <div className="bg-white border border-[#2D4944] rounded-2xl flex flex-col ">
+
+                            <div className="flex flex-col h-36 items-center justify-center">
+                                <img className="object-cover object-center w-full h-full rounded-t-2xl" src={product.url} alt=""/>
+                            </div>
+
+                                <div className="flex justify-between items-center">
+                                    <div className="flex flex-col gap-1 p-4 pl-6 truncate">
+                                        <p className="text-[#2D4944] text-xl font-bold truncate ">{product.ptitle}</p>
+                                        <p className="text-[#6b9089] text-xl font-bold truncate ">₱{product.price}.00</p>
+                                        <p className="text-[#6b6b6b] text-base truncate">Quantity: {product.quantity}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button className="bg-[#2D4944] h-12 w-full mt-4 text-white rounded-lg px-6 p-2 hover:bg-[#DCE0DC] hover:text-[#2D4944]">Buy Now</button>
+                            <button></button>
+                        </div>
+                ))}
             </div>
 
 
