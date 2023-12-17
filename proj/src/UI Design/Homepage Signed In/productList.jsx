11c4257e79df016b296da4bc158027c683c6634a
rdnieto <rdnieto1@up.edu.ph>
2023-12-17 20:01:44 +0800

    Update productList.jsx

--- proj/src/UI Design/Homepage Signed In/productList.jsx
+++ proj/src/UI Design/Homepage Signed In/productList.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 
 const Products = () => {
 
-
+    const [sortState, setSortState] = useState("none");
     const [ products, setProducts] = useState([])
     //const [ greeting, setGreeting ] = useState('')
     useEffect(() => {
@@ -13,6 +13,23 @@ const Products = () => {
       })
     }, [])
 
+    const sortMethods = {
+        none: { method: null},
+        ascendingtype: { method: (a, b) => (a.ptype - b.ptype) },
+        descendingtype: { method: (a, b) => (b.ptype - a.ptype) },
+        ascendingquantity: { method: (a, b) => (a.quantity - b.quantity)},
+        descendingquantity: { method: (a, b) => (b.quantity - a.quantity) },
+        ascendingname: { method: (a, b) => (a.ptitle > b.ptitle ? 1 : -1)  },
+        descendingname: { method: (a, b) => (a.ptitle > b.ptitle ? -1 : 1) },
+      };
+
+    const productDisplay = (sortstate) =>{
+        if(sortstate ==="none"){
+            return [...products];
+        }else{
+            return [...products].sort(sortMethods[sortstate].method);
+        }
+    }
     return(
         <div>
 
@@ -32,13 +49,25 @@ const Products = () => {
                     <button className="bg-[#2D4944] font-bold h-10 w-24 text-sm text-white rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">Default</button>
                     <button className="bg-white font-bold h-10 w-24 border border-[#2D4944] text-sm text-[#2D4944] rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">A-Z</button>
                     <button className="bg-white font-bold h-10 w-24 border border-[#2D4944] text-sm text-[#2D4944] rounded-full p-1 hover:bg-[#DCE0DC] hover:text-[#2D4944]">Price Cost</button>
+                    <select defaultValue={'DEFAULT'} onChange={(e) => setSortState(e.target.value)}>
+                        <option value="DEFAULT" disabled>None</option>
+                        <option value="none" >Default</option>
+                        <option value="ascendingtype">Ascending by type</option>
+                        <option value="descendingtype">Descending by type</option>
+                        <option value="ascendingquantity">Ascending by quantity</option>
+                        <option value="descendingquantity">Descending by quantity</option>
+                        <option value="ascendingname">Ascending by name</option>
+                        <option value="descendingname">Descending by name</option>
+                    </select>
+
                 </div>
             </div>
 
 
             <div>
                 {
-                products.map((product, index) => (
+                
+                productDisplay(sortState).map((product, index) => (
                         <div key={product.ptitle}>
                             <div className="bg-white border border-[#2D4944] rounded-2xl flex flex-col ">
 
